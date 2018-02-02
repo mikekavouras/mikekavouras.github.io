d0419c73ba26f2ec7adf8b9b5b59f8b231af6402
Mike Kavouras <kavourasm@gmail.com>
2018-02-01 23:08:09 -0600

    paul

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// video source: https://vimeo.com/90312869
+var img;
+var vid;
+var theta = 0;
+
+function setup(){
+  createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+
+  vid = createVideo(["assets/paul.mp4"]);
+  vid.loop();
+  vid.hide();
+}
+
+function draw(){
+  var size = 400;
+  background(255);
+  texture(vid);
+  push();
+    rotateZ(theta * 0.1);
+    rotateX(theta * 0.1);
+    rotateY(theta * 0.1);
+    box(size, size, size);
+  pop();
+  theta += 0.05;
+}
